5e5e5d21b015c8f35b43fd45c3928dee5f291ab3
Joe Lapp <arachnojoe@gmail.com>
2022-03-17 21:51:25 -0500

    Working on Location, not yet passing tests

---
+++ src/database/location.test.ts
@@ -0,0 +1,205 @@
+import { test, expect } from '@playwright/test';
+
+import type { DB } from '../util/pg_util';
+import { initTestDatabase } from '../util/test_util';
+import { Location, LocationType } from './location';
+
+let db: DB;
+
+test.beforeAll(async () => {
+  db = await initTestDatabase();
+});
+
+test('series of sequentially dependent location tests', async () => {
+  // Each of these tests depends on the prior tests, so run all as a unit.
+
+  // test adding continent location
+
+  {
+    const locationName = 'North America';
+    const sourceLocation = {
+      locationType: LocationType.Continent,
+      locationName,
+      publicLatitude: null,
+      publicLongitude: null,
+      parentID: null
+    };
+    const expectedLocation = Object.assign(
+      { locationID: 1, parentIDSeries: null, parentNameSeries: null },
+      sourceLocation
+    );
+    const createdLocation = await Location.create(db, null, null, sourceLocation);
+    expect(createdLocation).toEqual(expectedLocation);
+    const readLocation = await Location.getByID(db, createdLocation.locationID);
+    expect(readLocation).toEqual(expectedLocation);
+  }
+
+  // test adding country of existing continent
+
+  {
+    const locationName = 'United States';
+    const sourceLocation = {
+      locationType: LocationType.Country,
+      locationName,
+      publicLatitude: null,
+      publicLongitude: null,
+      parentID: 1
+    };
+    const expectedLocation = Object.assign(
+      { locationID: 2, parentIDSeries: '1', parentNameSeries: 'North America' },
+      sourceLocation
+    );
+    const createdLocation = await Location.create(
+      db,
+      'North America',
+      '1',
+      sourceLocation
+    );
+    expect(createdLocation).toEqual(expectedLocation);
+    const readLocation = await Location.getByID(db, createdLocation.locationID);
+    expect(readLocation).toEqual(expectedLocation);
+  }
+
+  // test getOrCreate() gets an existing location
+
+  {
+    const locationName = 'United States';
+    const expectedLocation = await Location.getByID(db, 2);
+    expect(expectedLocation?.locationName).toEqual(locationName);
+    const readLocation = await Location.getOrCreate(db, {
+      continent: 'North America',
+      country: 'United States'
+    });
+    expect(readLocation).toEqual(expectedLocation);
+  }
+
+  // test auto-creating new location having no intermediates
+
+  {
+    const locationName = 'Texas';
+    const createdLocation = await Location.getOrCreate(db, {
+      continent: 'North America',
+      country: 'United States',
+      stateProvince: 'Texas'
+    });
+    expect(createdLocation).toEqual({
+      locationID: 3,
+      locationType: LocationType.StateProvince,
+      locationName,
+      publicLatitude: null,
+      publicLongitude: null,
+      parentID: 2,
+      parentIDSeries: '1,2',
+      parentNameSeries: 'North America|United States'
+    });
+  }
+
+  // test auto-creating new locality and new intermediate taxon
+
+  {
+    const locationName = 'Missing Cave';
+    const createdLocation = await Location.getOrCreate(db, {
+      continent: 'North America',
+      country: 'United States',
+      stateProvince: 'Texas',
+      county: 'Travis County',
+      locality: locationName,
+      publicLatitude: 28.12,
+      publicLongitude: -97.34
+    });
+    expect(await Location.getByID(db, 4)).toEqual({
+      locationID: 4,
+      locationType: LocationType.County,
+      locationName: 'Travis County',
+      publicLatitude: null,
+      publicLongitude: null,
+      parentID: 3,
+      parentIDSeries: '1,2,3',
+      parentNameSeries: 'North America|United States|Texas'
+    });
+    const readLocation = await Location.getByID(db, 5);
+    expect(readLocation).toEqual({
+      locationID: 5,
+      locationType: LocationType.Locality,
+      locationName,
+      publicLatitude: 28.12,
+      publicLongitude: -97.34,
+      parentID: 4,
+      parentIDSeries: '1,2,3,4',
+      parentNameSeries: 'North America|United States|Texas|Travis County'
+    });
+    expect(createdLocation).toEqual(readLocation);
+  }
+
+  // test creating multiple intermediate localities
+
+  {
+    const createdLocation = await Location.getOrCreate(db, {
+      continent: 'North America',
+      country: 'Mexico',
+      stateProvince: 'Chihuahua',
+      county: 'Mun. Xyz',
+      locality: 'Invisible Spring',
+      publicLatitude: 21.12,
+      publicLongitude: -96.34
+    });
+    expect(await Location.getByID(db, 6)).toEqual({
+      locationID: 6,
+      locationType: LocationType.Country,
+      locationName: 'Mexico',
+      publicLatitude: null,
+      publicLongitude: null,
+      parentID: 1,
+      parentIDSeries: '1',
+      parentNameSeries: 'North America'
+    });
+    expect(await Location.getByID(db, 7)).toEqual({
+      locationID: 7,
+      locationType: LocationType.StateProvince,
+      locationName: 'Chihuahua',
+      publicLatitude: null,
+      publicLongitude: null,
+      parentID: 6,
+      parentIDSeries: '1,6',
+      parentNameSeries: 'North America|Mexico'
+    });
+    expect(await Location.getByID(db, 8)).toEqual({
+      locationID: 8,
+      locationType: LocationType.County,
+      locationName: 'Mun. Xyz',
+      publicLatitude: null,
+      publicLongitude: null,
+      parentID: 7,
+      parentIDSeries: '1,6,7',
+      parentNameSeries: 'North America|Mexico|Chihuahua'
+    });
+    const readLocation = await Location.getByID(db, 9);
+    expect(readLocation).toEqual({
+      locationID: 9,
+      locationType: LocationType.Locality,
+      locationName: 'Invisible Spring',
+      publicLatitude: 21.12,
+      publicLongitude: -96.34,
+      parentID: 8,
+      parentIDSeries: '1,6,7,8',
+      parentNameSeries: 'North America|Mexico|Chihuahua|Mun. Xyz'
+    });
+    expect(createdLocation).toEqual(readLocation);
+  }
+
+  // test providing the public coordinates name of an existing location
+
+  {
+    const expectedLocation = await Location.getByID(db, 4);
+    expect(expectedLocation?.locationName).toEqual('Travis County');
+    expectedLocation!.publicLatitude = 23.5;
+    expectedLocation!.publicLongitude = -97.5;
+    await expectedLocation!.save(db);
+    const readLocation = await Location.getByID(db, 4);
+    expect(readLocation).toEqual(expectedLocation);
+  }
+});
+
+test.afterAll(async () => {
+  await db.close();
+});
--- src/database/location.ts
+++ src/database/location.ts
@@ -1,46 +1,268 @@
-import type { Client } from 'pg';
+/**
+ * Class Location represents a location of the database locations table
+ * and provides access to the table. The purpose of this table is to
+ * provide easy-access to the location hierarchy.
+ */
 
-import type { PublicLocationsRow } from './schema';
+// NOTE: This module is analogous to taxon.ts, but I did not create a
+// generic type for them because it would be a complicated type, and I
+// wanted to be sure the code was accessible to TypeScript newbies. Hence,
+// any logic you change here many need to change in taxon.ts.
 
-export class Location implements PublicLocationsRow {
-	locationID = 0;
-	continentName: string;
-	countryName: string;
-	stateName: string;
-	countyName: string | null;
-	locationName: string | null;
-	impreciseLatitude: number;
-	impreciseLongitude: number;
+import type { DataOf } from '../util/type_util';
+import { DB, toCamelRow } from '../util/pg_util';
 
-	constructor(row: Omit<PublicLocationsRow, 'locationID'>) {
-		this.continentName = row.continentName;
-		this.countryName = row.countryName;
-		this.stateName = row.stateName;
-		this.countyName = row.countyName;
-		this.locationName = row.locationName;
-		this.impreciseLatitude = row.impreciseLatitude;
-		this.impreciseLongitude = row.impreciseLongitude;
-	}
+export type LocationData = DataOf<Location>;
 
-	async save(db: Client): Promise<number> {
-		const result = await db.query(
-			`insert into public_locations(
-          continentName, countryName,
-          stateName, countyName, locationName,
-          impreciseLatitude, impreciseLongitude
-        ) values ($1, $2, $3, $4, $5, $6, $7)
-        returning locationID`,
-			[
-				this.continentName,
-				this.countryName,
-				this.stateName,
-				this.countyName,
-				this.locationName,
-				this.impreciseLatitude,
-				this.impreciseLongitude
-			]
-		);
-		this.locationID = result.rows[0].locationID;
-		return this.locationID;
-	}
+export enum LocationType {
+  Continent = 'continent',
+  Country = 'country',
+  StateProvince = 'stateProvince',
+  County = 'county',
+  Locality = 'locality'
+}
+
+const orderedTypes = [
+  LocationType.Continent,
+  LocationType.Country,
+  LocationType.StateProvince,
+  LocationType.County,
+  LocationType.Locality
+];
+
+export interface LocationSource {
+  continent: string;
+  country?: string;
+  stateProvince?: string;
+  county?: string;
+  locality?: string;
+  publicLatitude?: number;
+  publicLongitude?: number;
+  parentNameSeries?: string;
+}
+
+interface LocationSpec {
+  locationType: LocationType;
+  locationName: string;
+  publicLatitude: number | null;
+  publicLongitude: number | null;
+  parentNameSeries: string | null;
+}
+
+export class Location {
+  locationID = 0;
+  locationType: LocationType;
+  locationName: string;
+  publicLatitude: number | null;
+  publicLongitude: number | null;
+  parentID: number | null;
+  parentIDSeries: string | null;
+  parentNameSeries: string | null;
+
+  //// CONSTRUCTION //////////////////////////////////////////////////////////
+
+  constructor(row: LocationData) {
+    this.locationID = row.locationID;
+    this.locationType = row.locationType;
+    this.locationName = row.locationName;
+    this.publicLatitude = row.publicLatitude;
+    this.publicLongitude = row.publicLongitude;
+    this.parentID = row.parentID;
+    this.parentIDSeries = row.parentIDSeries;
+    this.parentNameSeries = row.parentNameSeries;
+  }
+
+  //// PUBLIC INSTANCE METHODS //////////////////////////////////////////////
+
+  async save(db: DB): Promise<number> {
+    if (this.locationID === 0) {
+      const result = await db.query(
+        `insert into locations(
+						location_type, location_name, public_latitude, public_longitude,
+						parent_id, parent_id_series, parent_name_series
+					) values ($1, $2, $3, $4, $5, $6, $7)
+					returning location_id`,
+        [
+          this.locationType,
+          this.locationName,
+          this.publicLatitude,
+          this.publicLongitude,
+          this.parentID,
+          this.parentIDSeries,
+          this.parentNameSeries
+        ]
+      );
+      this.locationID = result.rows[0].location_id;
+    } else {
+      const result = await db.query(
+        `update locations set 
+						location_type=$1, location_name=$2, public_latitude=$3, public_longitude=$4,
+						parent_id=$5, parent_id_series=$6, parent_name_series=$7
+					where location_id=$8`,
+        [
+          this.locationType,
+          this.locationName,
+          this.publicLatitude,
+          this.publicLongitude,
+          this.parentID,
+          this.parentIDSeries,
+          this.parentNameSeries,
+          this.locationID
+        ]
+      );
+      if (result.rowCount != 1) {
+        throw Error(`Failed to update locality ID ${this.locationID}`);
+      }
+    }
+    return this.locationID;
+  }
+
+  //// PUBLIC CLASS METHODS //////////////////////////////////////////////////
+
+  static async create(
+    db: DB,
+    parentNameSeries: string | null,
+    parentIDSeries: string | null,
+    data: Omit<LocationData, 'locationID' | 'parentIDSeries' | 'parentNameSeries'>
+  ): Promise<Location> {
+    const location = new Location(
+      Object.assign(
+        {
+          locationID: 0 /* DB will assign a value */,
+          parentIDSeries,
+          parentNameSeries
+        },
+        data
+      )
+    );
+    await location.save(db);
+    return location;
+  }
+
+  static async getByID(db: DB, locationID: number): Promise<Location | null> {
+    const result = await db.query(`select * from locations where location_id=$1`, [
+      locationID
+    ]);
+    return result.rows.length > 0 ? new Location(toCamelRow(result.rows[0])) : null;
+  }
+
+  static async getOrCreate(db: DB, source: LocationSource): Promise<Location> {
+    // Return the location if it already exists.
+
+    const [parentLocations, locationName] = Location._parseLocationSpec(source);
+    let location = await Location._getByNameSeries(
+      db,
+      parentLocations.join('|'),
+      locationName
+    );
+    if (location) return location;
+
+    // If the location doesn't exist yet, create specs for all its ancestors.
+
+    const specs: LocationSpec[] = [];
+    let parentNameSeries: string | null = null;
+    for (let i = 0; i < parentLocations.length; ++i) {
+      const ancestorName = parentLocations[i];
+      specs.push({
+        locationType: orderedTypes[i],
+        locationName: ancestorName,
+        publicLatitude: null,
+        publicLongitude: null,
+        parentNameSeries
+      });
+      if (parentNameSeries == null) {
+        parentNameSeries = ancestorName; // necessarily continent
+      } else {
+        parentNameSeries += '|' + ancestorName;
+      }
+    }
+
+    // Create a spec for the particular requested location.
+
+    specs.push({
+      locationType: orderedTypes[parentLocations.length],
+      locationName,
+      publicLatitude: source.publicLatitude || null,
+      publicLongitude: source.publicLongitude || null,
+      parentNameSeries
+    });
+
+    // Create all implied locations.
+
+    return await Location._createMissingLocations(db, specs);
+  }
+
+  //// PRIVATE CLASS METHDOS /////////////////////////////////////////////////
+
+  private static async _createMissingLocations(
+    db: DB,
+    specs: LocationSpec[]
+  ): Promise<Location> {
+    let [location, locationIndex] = await Location._getClosestLocation(
+      db,
+      specs,
+      specs.length - 1 // nearest to the last specified location
+    );
+    let parentIDSeries = location?.parentIDSeries || null;
+    while (++locationIndex < specs.length) {
+      if (location) {
+        if (parentIDSeries == null) {
+          parentIDSeries = location.locationID.toString();
+        } else {
+          parentIDSeries += ',' + location.locationID.toString();
+        }
+      }
+      const spec = specs[locationIndex];
+      location = await Location.create(db, spec.parentNameSeries, parentIDSeries, {
+        locationType: spec.locationType,
+        locationName: spec.locationName,
+        publicLatitude: spec.publicLatitude,
+        publicLongitude: spec.publicLongitude,
+        parentID: location?.locationID || null
+      });
+    }
+    return location!;
+  }
+
+  private static async _getByNameSeries(
+    db: DB,
+    parentNameSeries: string | null,
+    locationName: string
+  ): Promise<Location | null> {
+    const result = await db.query(
+      `select * from locations where parent_name_series=$1 and location_name=$2`,
+      [parentNameSeries, locationName]
+    );
+    return result.rows.length > 0 ? new Location(toCamelRow(result.rows[0])) : null;
+  }
+
+  private static async _getClosestLocation(
+    db: DB,
+    specs: LocationSpec[],
+    specIndex: number
+  ): Promise<[Location | null, number]> {
+    const spec = specs[specIndex];
+    const location = await Location._getByNameSeries(
+      db,
+      spec.parentNameSeries,
+      spec.locationName
+    );
+    if (location) {
+      return [location, specIndex];
+    }
+    if (specIndex == 0) {
+      return [null, -1];
+    }
+    return Location._getClosestLocation(db, specs, specIndex - 1);
+  }
+
+  private static _parseLocationSpec(source: LocationSource): [string[], string] {
+    const parentLocations: string[] = [source.continent];
+    if (source.country) parentLocations.push(source.country);
+    if (source.stateProvince) parentLocations.push(source.stateProvince);
+    if (source.county) parentLocations.push(source.county);
+    if (source.locality) parentLocations.push(source.locality);
+    const locationName = parentLocations.pop();
+    return [parentLocations, locationName!];
+  }
 }
