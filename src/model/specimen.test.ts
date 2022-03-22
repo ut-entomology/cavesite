7b3e874fcde5cb3889019f8ba35525bc1332c633
Joe Lapp <arachnojoe@gmail.com>
2022-03-22 11:34:07 -0500

    Added test of partial specimen

--- src/model/specimen.test.ts
+++ src/model/specimen.test.ts
@@ -3,8 +3,8 @@ import { test, expect } from '@playwright/test';
 import type { DB } from '../util/pg_util';
 import { initTestDatabase } from '../util/test_util';
 import { Specimen } from './specimen';
-import { Location, LocationType } from './location';
-import { Taxon, TaxonRank } from './taxon';
+import { Location } from './location';
+import { Taxon } from './taxon';
 
 const TZ_SUFFIX = 'T06:00:00.000Z';
 
@@ -15,13 +15,14 @@ test.beforeAll(async () => {
 });
 
 test('creating a fully-specified specimen', async () => {
+  const startDate = new Date('2020-01-01' + TZ_SUFFIX);
+  const endDate = new Date('2020-01-04' + TZ_SUFFIX);
+  const endDateISO = endDate.toISOString();
+  const detDate = new Date('2020-06-10' + TZ_SUFFIX);
+
   // test creating a fully-specified specimen
 
   {
-    const startDate = new Date('2020-01-01' + TZ_SUFFIX);
-    const endDate = new Date('2020-01-04' + TZ_SUFFIX);
-    const endDateISO = endDate.toISOString();
-    const detDate = new Date('2020-06-10' + TZ_SUFFIX);
     const source = {
       catalogNumber: 'C1',
       occurrenceID: 'X1',
@@ -94,6 +95,65 @@ test('creating a fully-specified specimen', async () => {
     expect((await Location.getByID(db, 1))?.locationName).toEqual('North America');
     expect((await Location.getByID(db, 3))?.locationName).toEqual('Texas');
     expect((await Location.getByID(db, 5))?.locationName).toEqual('My backyard');
+  }
+
+  // test creating a partially-specified specimen in existing hierarchy
+
+  {
+    const source = {
+      catalogNumber: 'C2',
+      occurrenceID: 'X2',
+
+      kingdom: 'Animalia',
+      phylum: 'Arthropoda',
+      class: 'Arachnida',
+      order: 'Araneae',
+      family: 'Thomisidae',
+      scientificName: 'Thomisidae',
+
+      continent: 'North America',
+      country: 'United States',
+      stateProvince: 'Texas',
+      locality: 'Their backyard',
+
+      startDate: startDate.toString(),
+      collectors: 'Person A',
+      determiners: 'Person C'
+    };
+    const specimen = await Specimen.create(db, source);
+    expect(specimen).toEqual({
+      catalogNumber: source.catalogNumber,
+      occurrenceGuid: source.occurrenceID,
+      kingdomID: 1,
+      phylumID: 2,
+      classID: 3,
+      orderID: 4,
+      familyID: 8,
+      genusID: null,
+      speciesID: null,
+      subspeciesID: null,
+      preciseTaxonID: 8,
+
+      continentID: 1,
+      countryID: 2,
+      stateProvinceID: 3,
+      countyID: null,
+      localityID: 6,
+      preciseLocationID: 6,
+
+      collectionStartDate: startDate,
+      collectionEndDate: null,
+      collectors: 'Person A',
+      determinationDate: null,
+      determiners: 'Person C',
+      collectionRemarks: null,
+      occurrenceRemarks: null,
+      determinationRemarks: null,
+      typeStatus: null,
+      specimenCount: null
+    });
+    expect((await Taxon.getByID(db, 8))?.taxonName).toEqual('Thomisidae');
+    expect((await Location.getByID(db, 6))?.locationName).toEqual('Their backyard');
   }
 });
 
