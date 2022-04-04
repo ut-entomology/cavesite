c5b2ac561a1e1c4e98ef5452d6938250e7bc1bfd
Joe Lapp <arachnojoe@gmail.com>
2022-04-04 10:05:12 -0500

    Forgot to save for prior commit

---
+++ src/components/AdminTabRoute.svelte
@@ -0,0 +1,28 @@
+<script lang='ts'>
+  import RouteTab, { type Tab } from "./RouteTab.svelte";
+
+  export let activeTab: string;
+
+  const tabs: Tab[] = [
+    {
+      label: 'Users',
+      route: '/admin/users'
+    },
+    {
+      label: 'Private Coordinates',
+      route: '/admin/coords'
+    },
+    {
+      label: 'Download Schedule',
+      route: '/admin/schedule'
+    },
+    {
+      label: 'Logs',
+      route: '/admin/logs'
+    },
+  ];
+</script>
+
+<RouteTab {tabs} {activeTab}>
+  <slot></slot>
+</RouteTab>
