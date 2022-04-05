cb2f5a299d26b5eb03a68b75074c4b921192a36c
Joe Lapp <arachnojoe@gmail.com>
2022-04-04 21:59:55 -0500

    Conditionally show admin tabs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,15 @@
 <script lang='ts'>
+  import { usingAdminTabs } from '../stores/usingAdminTabs';
+  import AdminTabRoute from '../components/AdminTabRoute.svelte';
   import DataTabRoute from '../components/DataTabRoute.svelte';
 </script>
 
-<DataTabRoute activeTab='Welcome'>
-  About this web site.
-</DataTabRoute>
+{#if $usingAdminTabs}
+  <AdminTabRoute activeTab='Users'>
+    Users
+  </AdminTabRoute>
+{:else}
+  <DataTabRoute activeTab='Welcome'>
+    About this web site.
+  </DataTabRoute>
+{/if}
