4523b89dbd6258149bb11b4422561a96733dcbbe
Joe Lapp <arachnojoe@gmail.com>
2022-04-25 15:32:08 -0500

    Corrected for first login

--- src/frontend/dialogs/LoginInfoDialog.svelte
+++ src/frontend/dialogs/LoginInfoDialog.svelte
@@ -2,13 +2,14 @@
   import { currentDialog } from '../stores/currentDialog.svelte';
   import ModalDialog from '../common/ModalDialog.svelte';
   import { userInfo } from '../stores/user_info';
+  import { DialogSpec } from '../common/VariableDialog.svelte';
 
   $: userName = $userInfo!.firstName
     ? $userInfo!.firstName + ' ' + $userInfo!.lastName
     : $userInfo!.lastName;
-  function requestReset() {
+  function changePassword() {
     closeDialog();
-    //$currentDialog = TBD;
+    $currentDialog = new DialogSpec('ChangePasswordDialog');
   }
 
   function closeDialog() {
@@ -16,13 +17,17 @@
   }
 </script>
 
-<ModalDialog title="Login Information" contentClasses="login-info-content">
-  <div class="login-info alert-warning pt-3 pb-3">
-    {#if $userInfo?.priorLoginDate}
-      <div class="row mb-4">
+{#if $userInfo}
+  <ModalDialog title="Login Information" contentClasses="login-info-content">
+    <div class="login_info alert-warning pt-3 pb-3">
+      <div class="row">
         <div>
-          Your are logged in as<br /><span class="user_name">{userName}</span>
-          {#if $userInfo.affiliation}<br />{$userInfo.affiliation}{/if}
+          You are logged in as
+          <div class="user_info">
+            <span class="user_name">{userName}</span><br />
+            {#if $userInfo.affiliation}{$userInfo.affiliation}<br />{/if}
+            {$userInfo.email}
+          </div>
         </div>
       </div>
       <div class="row mb-4">
@@ -30,34 +35,34 @@
           at IP address <span class="ip">{$userInfo?.lastLoginIP}</span>.
         </div>
       </div>
-      <div class="row mb-2">
-        <div>
-          You previous login was on<br />{new Date(
-            $userInfo.priorLoginDate
-          ).toLocaleString()}.<br />
-          {#if $userInfo.priorLoginIP == $userInfo.lastLoginIP}
-            from the same IP address.
-          {:else}
-            from IP <span class="ip">{$userInfo.priorLoginIP}</span>.
-          {/if}
+      {#if $userInfo.priorLoginDate}
+        <div class="row mb-2">
+          <div>
+            You previous login was on<br />{new Date(
+              $userInfo.priorLoginDate
+            ).toLocaleString()}.<br />
+            {#if $userInfo.priorLoginIP == $userInfo.lastLoginIP}
+              from the same IP address.
+            {:else}
+              from IP <span class="ip">{$userInfo.priorLoginIP}</span>.
+            {/if}
+          </div>
         </div>
-      </div>
-    {:else}
-      <div class="row mb-2"><div>This is your first login.</div></div>
-      <div class="row">
-        <div>Your IP address is <span class="ip">{$userInfo?.lastLoginIP}</span>.</div>
-      </div>
-    {/if}
-  </div>
-  <div class="row g-2">
-    <div class="col-12 text-center">
-      <button class="btn btn-minor" type="button" on:click={requestReset}
-        >Request Password Reset</button
-      >
-      <button class="btn btn-major" type="submit" on:click={closeDialog}>Close</button>
+      {:else}
+        <div class="row mb-2"><div>This is your first login.</div></div>
+      {/if}
     </div>
-  </div>
-</ModalDialog>
+    <div class="row g-2">
+      <div class="col-12 text-center">
+        <button class="btn btn-minor" type="button" on:click={changePassword}
+          >Change Password</button
+        >
+        <button class="btn btn-major" type="submit" on:click={closeDialog}>Close</button
+        >
+      </div>
+    </div>
+  </ModalDialog>
+{/if}
 
 <style lang="scss">
   @import '../variables.scss';
@@ -67,9 +72,13 @@
     max-width: 28rem;
   }
 
-  .login-info {
+  .login_info {
     border-radius: $border-radius;
     text-align: center;
+  }
+
+  .user_info {
+    margin: 0.75rem 0;
   }
 
   span.user_name {
