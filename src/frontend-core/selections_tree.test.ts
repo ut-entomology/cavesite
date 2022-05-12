cd28a07f3b36651fc5844b54335e232dfc6830d2
Joe Lapp <arachnojoe@gmail.com>
2022-05-12 09:40:57 -0500

    Rewrote using recursion to pass all existing tests

--- src/frontend-core/selections_tree.test.ts
+++ src/frontend-core/selections_tree.test.ts
@@ -22,7 +22,10 @@ class TestTree extends SelectionsTree<TestSpec> {
 
 const specA = new TestSpec('A');
 const specAA = new TestSpec('AA', [specA]);
+const specAAA = new TestSpec('AAA', [specA, specAA]);
+const specAAB = new TestSpec('AAB', [specA, specAA]);
 const specAB = new TestSpec('AB', [specA]);
+const specABA = new TestSpec('ABA', [specA, specAB]);
 
 test('creating and using an empty tree', () => {
   const tree = new TestTree([]);
@@ -38,13 +41,13 @@ test('adding and removing a root element', () => {
   expect(tree.getSelections()).toEqual(['A']);
   expect(tree.isSelected('A')).toBe(true);
 
-  tree.removeSelection([{ spec: specA, children: [] }]);
+  tree.removeSelection([], specA);
   expect(tree.getRootNode()).toBeNull();
   expect(tree.getSelections()).toEqual([]);
   expect(tree.isSelected('A')).toBe(false);
 });
 
-test('adding and removing two-deep nested elements', () => {
+test('adding and removing 2-deep nested elements', () => {
   const tree = new TestTree([]);
   tree.addSelection(specA);
 
@@ -70,21 +73,86 @@ test('adding and removing two-deep nested elements', () => {
   expect(tree.isSelected('AA')).toBe(true);
   expect(tree.isSelected('AB')).toBe(true);
 
-  tree.removeSelection([
-    { spec: specA, children: [specAB] },
-    { spec: specAB, children: [] }
-  ]);
+  tree.removeSelection([{ spec: specA, children: [specAB] }], specAB);
   expect(tree.getSelections()).toEqual(['AA']);
   expect(tree.isSelected('A')).toBe(false);
   expect(tree.isSelected('AA')).toBe(true);
   expect(tree.isSelected('AB')).toBe(false);
 
-  tree.removeSelection([
-    { spec: specA, children: [specAA] },
-    { spec: specAA, children: [] }
-  ]);
-  expect(tree.getSelections()).toEqual(['A']);
-  expect(tree.isSelected('A')).toBe(true);
+  tree.removeSelection([{ spec: specA, children: [specAA] }], specAA);
+  expect(tree.getSelections()).toEqual([]);
+  expect(tree.isSelected('A')).toBe(false);
   expect(tree.isSelected('AA')).toBe(false);
   expect(tree.isSelected('AB')).toBe(false);
+});
+
+test('adding and removing 3-deep nested elements', () => {
+  const tree = new TestTree([]);
+  tree.addSelection(specAA);
+  tree.addSelection(specAAB);
+  tree.addSelection(specAAA);
+  tree.addSelection(specABA);
+
+  expect(tree.getRootNode()).toEqual({
+    spec: specA,
+    children: [
+      {
+        spec: specAA,
+        children: [
+          { spec: specAAA, children: [] },
+          { spec: specAAB, children: [] }
+        ]
+      },
+      { spec: specAB, children: [{ spec: specABA, children: [] }] }
+    ]
+  });
+
+  expect(tree.getSelections().sort()).toEqual(['AAA', 'AAB', 'ABA']);
+  expect(tree.isSelected('A')).toBe(false);
+  expect(tree.isSelected('AA')).toBe(false);
+  expect(tree.isSelected('AAA')).toBe(true);
+  expect(tree.isSelected('AAB')).toBe(true);
+  expect(tree.isSelected('AB')).toBe(false);
+  expect(tree.isSelected('ABA')).toBe(true);
+
+  tree.removeSelection(
+    [
+      { spec: specA, children: [specAB] },
+      { spec: specAB, children: [specABA] }
+    ],
+    specABA
+  );
+  expect(tree.getSelections().sort()).toEqual(['AAA', 'AAB']);
+  expect(tree.isSelected('A')).toBe(false);
+  expect(tree.isSelected('AA')).toBe(false);
+  expect(tree.isSelected('AAA')).toBe(true);
+  expect(tree.isSelected('AAB')).toBe(true);
+  expect(tree.isSelected('AB')).toBe(false);
+  expect(tree.isSelected('ABA')).toBe(false);
+
+  tree.removeSelection(
+    [
+      { spec: specA, children: [specAA] },
+      { spec: specAA, children: [specAAA, specAAB] }
+    ],
+    specAAA
+  );
+  expect(tree.getSelections()).toEqual(['AAB']);
+  expect(tree.isSelected('A')).toBe(false);
+  expect(tree.isSelected('AA')).toBe(false);
+  expect(tree.isSelected('AAA')).toBe(false);
+  expect(tree.isSelected('AAB')).toBe(true);
+
+  tree.removeSelection(
+    [
+      { spec: specA, children: [specAA] },
+      { spec: specAA, children: [specAAB] }
+    ],
+    specAAB
+  );
+  expect(tree.getSelections()).toEqual([]);
+  expect(tree.isSelected('A')).toBe(false);
+  expect(tree.isSelected('AA')).toBe(false);
+  expect(tree.isSelected('AAA')).toBe(false);
+  expect(tree.isSelected('AAB')).toBe(false);
 });
--- src/frontend-core/selections_tree.ts
+++ src/frontend-core/selections_tree.ts
@@ -9,7 +9,7 @@ export interface Spec {
   unique: string;
 }
 
-export interface PathEntry<S extends Spec> {
+export interface SpecEntry<S extends Spec> {
   spec: S;
   children: S[];
 }
@@ -71,66 +71,61 @@ export abstract class SelectionsTree<S extends Spec> {
     return this._selectionsByUnique[unique] || false;
   }
 
-  removeSelection(pathToSpec: PathEntry<S>[]): void {
-    // Walk the selection tree as close to the to-be-removed entity as possible.
-
-    let parentNode: TreeNode<S> | null = null;
-    let node = this._rootNode; // deepest matching node
-    if (!node) throw Error('Attempted to remove from an empty set of selections');
-    let nextNode: TreeNode<S> | null = node; // next node whose children are to be examined
-    let pathIndex = 0; // index of next path entry to examine
-
-    while (nextNode) {
-      // The root node will match the first containing entity, when present.
-      node = nextNode;
-      nextNode = null;
-      if (++pathIndex < pathToSpec.length) {
-        const nextUnique = pathToSpec[pathIndex].spec.unique;
-        nextNode =
-          node.children.find((child) => child.spec.unique == nextUnique) || null;
-        parentNode = node;
-      }
-    }
-    if (node.children.length > 0) {
-      throw Error('Attempted to remove an unimplied selection');
-    }
-
-    // If we walked the entire path, remove the entity for the last node of the path.
-
-    if (pathIndex == pathToSpec.length) {
-      if (parentNode === null) {
+  removeSelection(containingSpecs: SpecEntry<S>[], spec: S): void {
+    if (!this._rootNode)
+      throw Error('Attempted to remove from an empty set of selections');
+    if (containingSpecs.length == 0) {
+      this._rootNode = null;
+    } else {
+      this._removeFromNode(this._rootNode, containingSpecs, spec);
+      if (this._rootNode.children.length == 0) {
         this._rootNode = null;
-      } else {
-        const nodeIndex = parentNode.children.indexOf(node);
-        parentNode.children.splice(nodeIndex, 1);
       }
     }
-
-    // Add all children outside the path below the previously-selected entity, so that
-    // removing an implied entity explicitly adds all previously implied entities but
-    // the one to be removed, below the nearest explicitly selected entity.
-    else {
-      nextNode = node;
-      while (pathIndex < pathToSpec.length) {
-        const nextDeeperUnique = pathToSpec[pathIndex].spec.unique;
-        node = nextNode;
-        for (const child of pathToSpec[pathIndex - 1].children) {
-          const childNode = { spec: child, children: [] };
-          node.children.push(childNode);
-          if (child.unique == nextDeeperUnique) {
-            nextNode = childNode;
-          }
-        }
-        node.children.sort(this._nodeSorter);
-        ++pathIndex;
-      }
-    }
-
     this._selectedUniques = null; // invalidate cached selections
   }
 
   private _nodeSorter(n1: TreeNode<S>, n2: TreeNode<S>) {
     return n1.spec.unique < n2.spec.unique ? -1 : 1;
+  }
+
+  private _removeChild(fromNode: TreeNode<S>, parentSpec: SpecEntry<S>, childSpec: S) {
+    const childUnique = childSpec.unique;
+    const childIndex = fromNode.children.findIndex((c) => c.spec.unique == childUnique);
+    if (childIndex >= 0) {
+      fromNode.children.splice(childIndex, 1);
+    } else {
+      for (const childSpec of parentSpec.children) {
+        if (childSpec.unique != childUnique) {
+          fromNode.children.push({ spec: childSpec, children: [] });
+        }
+      }
+      fromNode.children.sort();
+    }
+  }
+
+  private _removeFromNode(
+    containingNode: TreeNode<S>,
+    containingSpecs: SpecEntry<S>[],
+    specToRemove: S
+  ): void {
+    const containingSpec = containingSpecs.shift()!;
+
+    if (containingSpecs.length > 0) {
+      const childSpec = containingSpecs[0].spec;
+      const childUnique = childSpec.unique;
+      let childNode = containingNode.children.find((c) => c.spec.unique == childUnique);
+      if (!childNode) {
+        childNode = { spec: childSpec, children: [] };
+        containingNode.children.push(childNode);
+      }
+      this._removeFromNode(childNode, containingSpecs, specToRemove);
+      if (childNode.children.length == 0) {
+        this._removeChild(containingNode, containingSpec, childSpec);
+      }
+    } else {
+      this._removeChild(containingNode, containingSpec, specToRemove);
+    }
   }
 
   private _setDerivedValues() {
