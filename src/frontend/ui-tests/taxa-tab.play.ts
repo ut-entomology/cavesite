aae3aa9debc9f9f1a2aa6b9b898c2eefe14863d4
Joe Lapp <arachnojoe@gmail.com>
2022-06-09 18:18:57 -0500

    Tested basic taxa autocomplete list

--- src/frontend/ui-tests/taxa-tab.play.ts
+++ src/frontend/ui-tests/taxa-tab.play.ts
@@ -1,7 +1,48 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
+const autocompleteInput = '.autocomplete-input';
+const autocompleteList = '.autocomplete-list';
+const autocompleteListItem = '.autocomplete-list-item';
+
+test('test for initial state', async ({ page }) => {
   await page.goto('http://localhost/taxa');
+
   const title = page.locator('.tab_title');
-  await expect(title).toHaveText('Selected Taxa');
+  await expect(title).toContainText('Selected Taxa');
+
+  const mainText = page.locator('main');
+  await expect(mainText).toContainText('No taxa selected');
+
+  const initialInput = await page.inputValue(autocompleteInput);
+  expect(initialInput).toBeFalsy();
+});
+
+test('test for more and more specific autocomplete', async ({ page }) => {
+  await page.goto('http://localhost/taxa');
+
+  await page.fill(autocompleteInput, 'a');
+  let list = page.locator(autocompleteList);
+  await expect(list).not.toBeVisible();
+
+  await page.fill(autocompleteInput, 'ar');
+  await expect(list).toBeVisible();
+
+  await expect(list).toContainText('Arachnida');
+  await expect(list).toContainText('paraconcinna');
+  await expect(list).toContainText('Pardosa');
+
+  await page.fill(autocompleteInput, 'ara');
+  await expect(list).toContainText('Arachnida');
+  await expect(list).toContainText('paraconcinna');
+  await expect(list).not.toContainText('Pardosa');
+
+  await page.fill(autocompleteInput, 'arac');
+  await expect(list).toContainText('Arachnida');
+  await expect(list).toContainText('paraconcinna');
+  await expect(list).not.toContainText('Pardosa');
+
+  await page.fill(autocompleteInput, 'arachnida');
+  await expect(list).toContainText('Arachnida');
+  let items = page.locator(autocompleteListItem);
+  await expect(items).toHaveCount(1);
 });
