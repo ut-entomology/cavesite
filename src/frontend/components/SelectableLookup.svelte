01baae2d00bdf87e4834b5f0beb69a0025e4926e
Joe Lapp <arachnojoe@gmail.com>
2022-06-20 12:11:16 -0500

    Based TaxonLookup on a generalized SelectableLookup

---
+++ src/frontend/components/SelectableLookup.svelte
@@ -0,0 +1,203 @@
+<script lang="ts">
+  import ClearerAutoComplete from '../common/ClearerAutoComplete.svelte';
+  import SelectionButton from '../components/SelectionButton.svelte';
+  import CircleIconButton from './CircleIconButton.svelte';
+  import { errorReason } from '../stores/client';
+  import type {
+    SpecEntry,
+    AddSelection,
+    RemoveSelection,
+    SelectionsTree
+  } from '../../frontend-core/selections_tree';
+  import { showNotice } from '../common/VariableNotice.svelte';
+
+  interface Spec {
+    unique: string;
+    hasChildren: boolean | null;
+  }
+
+  const LOAD_DELAY_MILLIS = 333;
+  const loupeIcon = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+<g><path d="M497.938,430.063l-126.914-126.91C389.287,272.988,400,237.762,400,200C400,89.719,310.281,0,200,0
+		C89.719,0,0,89.719,0,200c0,110.281,89.719,200,200,200c37.762,0,72.984-10.711,103.148-28.973l126.914,126.91
+		C439.438,507.313,451.719,512,464,512c12.281,0,24.563-4.688,33.938-14.063C516.688,479.195,516.688,448.805,497.938,430.063z
+		M64,200c0-74.992,61.016-136,136-136s136,61.008,136,136s-61.016,136-136,136S64,274.992,64,200z"/></g></svg>`;
+
+  export let typeLabel: string;
+  export let selectionsTree: SelectionsTree<Spec>;
+  export let loadMatches: (partialName: string) => Promise<Spec[]>;
+  export let loadSpecIndicatingChildren: (unique: string) => Promise<Spec | null>;
+  export let getContainingSpecs: (
+    ofSpec: Spec,
+    includesGivenSpec: boolean
+  ) => Promise<SpecEntry<Spec>[]>;
+  export let createContainingSpecs: (spec: Spec) => Spec[];
+  export let toItemHtml: (spec: Spec, label: string) => string;
+  export let addSelection: AddSelection<Spec>;
+  export let removeSelection: RemoveSelection<Spec>;
+  export let openUnique: (selectedUnique: string) => Promise<void>;
+  export let setClearer: (clearer: () => void) => void;
+
+  interface MatchedItem {
+    unique: string;
+    spec: Spec;
+  }
+
+  let matchedSpecs: Spec[] = [];
+  let selection: string | undefined;
+  let isSelectedInTree = false;
+  let selectedSpec: Spec | null = null;
+  let specsByUnique: Record<string, Spec> = {};
+
+  $: if (selection && selection != '') {
+    selectedSpec = matchedSpecs.find((spec) => spec.unique == selection)!;
+    isSelectedInTree = selectionsTree.isSelected(selectedSpec.unique);
+    if (!isSelectedInTree) {
+      const containingSpecs = createContainingSpecs(selectedSpec);
+      for (const containingSpec of containingSpecs) {
+        if (selectionsTree.isSelected(containingSpec.unique)) {
+          isSelectedInTree = true;
+          break;
+        }
+      }
+    }
+  } else {
+    selectedSpec = null;
+  }
+
+  async function _loadMatches(partialName: string): Promise<MatchedItem[]> {
+    matchedSpecs = await loadMatches(partialName);
+    if (matchedSpecs!.length == 0) {
+      return [];
+    } else {
+      specsByUnique = {};
+      for (const spec of matchedSpecs!) {
+        specsByUnique[spec.unique] = spec;
+        if (spec.unique.toLocaleLowerCase() == partialName.toLocaleLowerCase()) {
+          selection = spec.unique;
+        }
+      }
+    }
+    return matchedSpecs
+      .map((spec) => {
+        return { unique: spec.unique, spec };
+      })
+      .sort((a, b) => (a.unique < b.unique ? -1 : 1));
+  }
+
+  async function _openSelectedSpec() {
+    const spec = await _loadSpecIndicatingChildren(selectedSpec!.unique);
+    if (spec) {
+      if (spec.hasChildren) {
+        openUnique(spec.unique);
+      } else {
+        const containingSpecs = createContainingSpecs(spec);
+        openUnique(containingSpecs[containingSpecs.length - 1].unique);
+      }
+    }
+  }
+
+  async function _addSelection() {
+    const spec = await _loadSpecIndicatingChildren(selectedSpec!.unique);
+    if (spec) {
+      addSelection(spec);
+      isSelectedInTree = true;
+    }
+  }
+
+  async function _loadSpecIndicatingChildren(unique: string): Promise<Spec | null> {
+    try {
+      return await loadSpecIndicatingChildren(unique);
+    } catch (err: any) {
+      showNotice({
+        header: 'ERROR',
+        alert: 'danger',
+        message: errorReason(err.response)
+      });
+      return null;
+    }
+  }
+
+  async function _removeSelection() {
+    const containingSpecs = await getContainingSpecs(selectedSpec!, false);
+    removeSelection(containingSpecs, selectedSpec!);
+    isSelectedInTree = false;
+  }
+</script>
+
+<div class="row justify-content-center gx-0">
+  <div class="col-sm-1 text-end auto_control">
+    {#if selectedSpec}
+      <SelectionButton
+        selected={isSelectedInTree}
+        addSelection={_addSelection}
+        removeSelection={_removeSelection}
+      />
+    {/if}
+  </div>
+  <div class="col selectable_autocomplete">
+    <ClearerAutoComplete
+      className="outer_auto_complete"
+      inputClassName="form-control"
+      bind:value={selection}
+      searchFunction={_loadMatches}
+      delay={LOAD_DELAY_MILLIS}
+      valueFieldName="unique"
+      labelFieldName="unique"
+      placeholder="Type a {typeLabel} to look up"
+      minCharactersToSearch={2}
+      hideArrow={true}
+      cleanUserText={false}
+      {setClearer}
+    >
+      <div slot="item" let:label let:item>{@html toItemHtml(item.spec, label)}</div>
+    </ClearerAutoComplete>
+  </div>
+  <div class="col-sm-1 auto_control">
+    {#if selectedSpec}
+      <CircleIconButton class="loupe_button" label="Open {typeLabel} in browser">
+        <div class="loupe_icon" on:click={_openSelectedSpec}>
+          {@html loupeIcon}
+        </div>
+      </CircleIconButton>
+    {/if}
+  </div>
+</div>
+
+<style lang="scss">
+  @import '../variables.scss';
+
+  .selectable_autocomplete :global(.autocomplete-list-item span) {
+    font-size: 0.8em;
+    color: #999;
+  }
+  .selectable_autocomplete :global(.autocomplete-list-item.selected span) {
+    color: #ddd;
+  }
+  :global(.outer_auto_complete) {
+    width: 100%;
+  }
+  :global(span.autocomplete-clear-button) {
+    opacity: 0.6;
+  }
+  .auto_control {
+    margin-top: 0.05rem;
+  }
+  :global(.loupe_button) {
+    margin-left: 0.5rem;
+    width: 1.5rem;
+    height: 1.5rem;
+    padding-left: 0.25rem;
+  }
+  .loupe_icon {
+    margin-top: -0.1rem;
+    width: 1rem;
+    height: 1rem;
+    fill: $blueLinkForeColor;
+    cursor: pointer;
+  }
+  .loupe_icon:hover {
+    fill: white;
+  }
+</style>
--- src/frontend/components/TaxonLookup.svelte
+++ src/frontend/components/TaxonLookup.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-  import ClearerAutoComplete from '../common/ClearerAutoComplete.svelte';
-  import SelectionButton from '../components/SelectionButton.svelte';
-  import CircleIconButton from './CircleIconButton.svelte';
-  import { client, errorReason } from '../stores/client';
+  import SelectableLookup from '../components/SelectableLookup.svelte';
+  import { client } from '../stores/client';
   import {
     ROOT_TAXON,
     type TaxonSpec,
@@ -17,15 +15,6 @@
     RemoveSelection
   } from '../../frontend-core/selections_tree';
   import type { TaxonSelectionsTree } from '../../frontend-core/taxon_selections_tree';
-  import { showNotice } from '../common/VariableNotice.svelte';
-
-  const LOAD_DELAY_MILLIS = 333;
-  const loupeIcon = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-<g><path d="M497.938,430.063l-126.914-126.91C389.287,272.988,400,237.762,400,200C400,89.719,310.281,0,200,0
-		C89.719,0,0,89.719,0,200c0,110.281,89.719,200,200,200c37.762,0,72.984-10.711,103.148-28.973l126.914,126.91
-		C439.438,507.313,451.719,512,464,512c12.281,0,24.563-4.688,33.938-14.063C516.688,479.195,516.688,448.805,497.938,430.063z
-		M64,200c0-74.992,61.016-136,136-136s136,61.008,136,136s-61.016,136-136,136S64,274.992,64,200z"/></g></svg>`;
 
   export let selectionsTree: TaxonSelectionsTree;
   export let getContainingTaxa: (
@@ -37,104 +26,29 @@
   export let openTaxon: (selection: string) => Promise<void>;
   export let setClearer: (clearer: () => void) => void;
 
-  interface MatchedItem {
-    unique: string;
-    spec: TaxonSpec;
-  }
+  const noTypeCheck = (x: any) => x;
 
-  let matchedSpecs: TaxonSpec[] = [];
-  let selection: string | undefined;
-  let isSelectedInTree = false;
-  let taxonSpec: TaxonSpec | null = null;
-  let specsByUnique: Record<string, TaxonSpec> = {};
-
-  $: if (selection && selection != '') {
-    taxonSpec = matchedSpecs.find((spec) => spec.unique == selection)!;
-    isSelectedInTree = selectionsTree.isSelected(taxonSpec.unique);
-    if (!isSelectedInTree) {
-      const containingSpecs = createContainingTaxonSpecs(taxonSpec);
-      for (const containingSpec of containingSpecs) {
-        if (selectionsTree.isSelected(containingSpec.unique)) {
-          isSelectedInTree = true;
-          break;
-        }
-      }
-    }
-  } else {
-    taxonSpec = null;
-  }
-
-  async function _loadMatches(partialName: string): Promise<MatchedItem[]> {
+  async function loadMatches(partialName: string): Promise<TaxonSpec[]> {
     let res = await $client.post('api/taxa/match_name', { partialName });
-    matchedSpecs = res.data.taxonSpecs;
+    const matchedSpecs: TaxonSpec[] = res.data.taxonSpecs;
     const rootSpecIndex = matchedSpecs.findIndex((spec) => spec.unique == ROOT_TAXON);
     if (rootSpecIndex >= 0) {
       matchedSpecs.splice(rootSpecIndex, 1);
     }
-    if (matchedSpecs!.length == 0) {
-      return [];
-    } else {
-      specsByUnique = {};
-      for (const spec of matchedSpecs!) {
-        specsByUnique[spec.unique] = spec;
-        if (spec.unique.toLocaleLowerCase() == partialName.toLocaleLowerCase()) {
-          selection = spec.unique;
-        }
-      }
-    }
-    return matchedSpecs
-      .map((spec) => {
-        return { unique: spec.unique, spec };
-      })
-      .sort((a, b) => (a.unique < b.unique ? -1 : 1));
+    return matchedSpecs;
   }
 
-  async function _openTaxon() {
-    const spec = await _loadSpecIndicatingChildren(taxonSpec!.unique);
-    if (spec) {
-      if (spec.hasChildren) {
-        openTaxon(spec.unique);
-      } else {
-        const containingSpecs = createContainingTaxonSpecs(spec);
-        openTaxon(containingSpecs[containingSpecs.length - 1].unique);
-      }
-    }
-  }
-
-  async function _addSelection() {
-    const spec = await _loadSpecIndicatingChildren(taxonSpec!.unique);
-    if (spec) {
-      addSelection(spec);
-      isSelectedInTree = true;
-    }
-  }
-
-  async function _loadSpecIndicatingChildren(
+  async function loadSpecIndicatingChildren(
     taxonUnique: string
   ): Promise<TaxonSpec | null> {
-    try {
-      let res = await $client.post('api/taxa/get_list', {
-        taxonUniques: [taxonUnique]
-      });
-      const taxonSpecs: TaxonSpec[] = res.data.taxonSpecs;
-      return taxonSpecs && taxonSpecs.length == 1 ? taxonSpecs[0] : null;
-    } catch (err: any) {
-      showNotice({
-        header: 'ERROR',
-        alert: 'danger',
-        message: errorReason(err.response)
-      });
-      return null;
-    }
+    let res = await $client.post('api/taxa/get_list', {
+      taxonUniques: [taxonUnique]
+    });
+    const taxonSpecs: TaxonSpec[] = res.data.taxonSpecs;
+    return taxonSpecs && taxonSpecs.length == 1 ? taxonSpecs[0] : null;
   }
 
-  async function _removeSelection() {
-    const containingSpecs = await getContainingTaxa(taxonSpec!, false);
-    removeSelection(containingSpecs, taxonSpec!);
-    isSelectedInTree = false;
-  }
-
-  function _toItemHtml(spec: TaxonSpec, label: string): string {
+  function toItemHtml(spec: TaxonSpec, label: string): string {
     let html = label;
     let rankIndex = taxonRanks.indexOf(spec.rank);
     if (italicRanks.includes(spec.rank)) {
@@ -153,78 +67,16 @@
   }
 </script>
 
-<div class="row justify-content-center gx-0">
-  <div class="col-sm-1 text-end auto_control">
-    {#if taxonSpec}
-      <SelectionButton
-        selected={isSelectedInTree}
-        addSelection={_addSelection}
-        removeSelection={_removeSelection}
-      />
-    {/if}
-  </div>
-  <div class="col auto_taxon">
-    <ClearerAutoComplete
-      className="outer_auto_complete"
-      inputClassName="form-control"
-      bind:value={selection}
-      searchFunction={_loadMatches}
-      delay={LOAD_DELAY_MILLIS}
-      valueFieldName="unique"
-      labelFieldName="unique"
-      placeholder="Type a taxon to look up"
-      minCharactersToSearch={2}
-      hideArrow={true}
-      cleanUserText={false}
-      {setClearer}
-    >
-      <div slot="item" let:label let:item>{@html _toItemHtml(item.spec, label)}</div>
-    </ClearerAutoComplete>
-  </div>
-  <div class="col-sm-1 auto_control">
-    {#if taxonSpec}
-      <CircleIconButton class="loupe_button" label="Open taxon in browser">
-        <div class="loupe_icon" on:click={_openTaxon}>
-          {@html loupeIcon}
-        </div>
-      </CircleIconButton>
-    {/if}
-  </div>
-</div>
-
-<style lang="scss">
-  @import '../variables.scss';
-
-  .auto_taxon :global(.autocomplete-list-item span) {
-    font-size: 0.8em;
-    color: #999;
-  }
-  .auto_taxon :global(.autocomplete-list-item.selected span) {
-    color: #ddd;
-  }
-  :global(.outer_auto_complete) {
-    width: 100%;
-  }
-  :global(span.autocomplete-clear-button) {
-    opacity: 0.6;
-  }
-  .auto_control {
-    margin-top: 0.05rem;
-  }
-  :global(.loupe_button) {
-    margin-left: 0.5rem;
-    width: 1.5rem;
-    height: 1.5rem;
-    padding-left: 0.25rem;
-  }
-  .loupe_icon {
-    margin-top: -0.1rem;
-    width: 1rem;
-    height: 1rem;
-    fill: $blueLinkForeColor;
-    cursor: pointer;
-  }
-  .loupe_icon:hover {
-    fill: white;
-  }
-</style>
+<SelectableLookup
+  typeLabel="taxon"
+  {selectionsTree}
+  {loadMatches}
+  {loadSpecIndicatingChildren}
+  getContainingSpecs={noTypeCheck(getContainingTaxa)}
+  createContainingSpecs={noTypeCheck(createContainingTaxonSpecs)}
+  toItemHtml={noTypeCheck(toItemHtml)}
+  {addSelection}
+  {removeSelection}
+  openUnique={openTaxon}
+  {setClearer}
+/>
