b1866cbe8941eb1f2fbc3a9fcc1a5d70281af0d8
Joe Lapp <arachnojoe@gmail.com>
2022-07-10 23:22:38 -0500

    Made responsive to changes in spec

--- src/frontend/components/EffortGraph.svelte
+++ src/frontend/components/EffortGraph.svelte
@@ -2,32 +2,32 @@
   import Scatter from 'svelte-chartjs/src/Scatter.svelte';
 
   import type { PlottableModel } from '../lib/plottable_model';
-  import type { EffortGraphSpec } from '../lib/effort_graphs';
   import type { SizedEffortGraphSpec } from '../lib/cluster_data';
 
   const POINTS_IN_MODEL_PLOT = 200;
 
   export let title: string;
-  export let spec: EffortGraphSpec | SizedEffortGraphSpec;
+  export let spec: SizedEffortGraphSpec;
   export let models: PlottableModel[] = [];
   export let yFormula: string | null = null;
 
-  const isSizedSpec = (spec as SizedEffortGraphSpec).graphSpecs !== undefined;
-  const xAxisText = isSizedSpec
-    ? (spec as SizedEffortGraphSpec).graphSpecs[0].xAxisLabel
-    : (spec as EffortGraphSpec).xAxisLabel;
-  const yAxisText = isSizedSpec
-    ? (spec as SizedEffortGraphSpec).graphSpecs[0].yAxisLabel
-    : (spec as EffortGraphSpec).yAxisLabel;
+  function _legendFilter(item: any) {
+    // When using a sized spec, show a legend for the first line and then
+    // one for each model; otherwise show all legends.
+    return (
+      item.datasetIndex == 0 ||
+      item.datasetIndex >= (spec as SizedEffortGraphSpec).graphSpecs.length
+    );
+  }
+</script>
 
-  const pointDatasets: any = [];
-  if (isSizedSpec) {
-    const sizedSpec = spec as SizedEffortGraphSpec;
-    pointDatasets.push(
-      ...sizedSpec.graphSpecs.map((graphSpec) => {
+<Scatter
+  data={{
+    datasets: [
+      ...spec.graphSpecs.map((graphSpec) => {
         return {
           showLine: true,
-          label: sizedSpec.pointCount + ' points',
+          label: spec.pointCount + ' points',
           data:
             models.length > 0
               ? models[0].convertDataPoints(graphSpec.points)
@@ -37,48 +37,7 @@
           hoverBorderWidth: 3,
           hoverBorderColor: '#000000'
         };
-      })
-    );
-  } else {
-    const unsizedSpec = spec as EffortGraphSpec;
-    pointDatasets.push({
-      label: unsizedSpec.points.length + ' points',
-      data:
-        models.length > 0
-          ? models[0].convertDataPoints(unsizedSpec.points)
-          : unsizedSpec.points,
-      // borderColor: _toLocationHexColor(i),
-      borderWidth: 1
-    });
-  }
-
-  function _legendFilter(item: any) {
-    // When using a sized spec, show a legend for the first line and then
-    // one for each model; otherwise show all legends.
-    return (
-      !isSizedSpec ||
-      item.datasetIndex == 0 ||
-      item.datasetIndex >= (spec as SizedEffortGraphSpec).graphSpecs.length
-    );
-  }
-
-  // @ts-ignore TypeScript doesn't recognize range of % operator.
-  // function _toLocationHexColor(i: number): string {
-  //   switch (i % 3) {
-  //     case 0:
-  //       return 'rgba(255, 0, 0, .3)';
-  //     case 1:
-  //       return 'rgba(0, 255, 0, .6)';
-  //     case 2:
-  //       return 'rgba(0, 0, 255, .3)';
-  //   }
-  // }
-</script>
-
-<Scatter
-  data={{
-    datasets: [
-      ...pointDatasets,
+      }),
       ...models.map((model) => {
         return {
           showLine: true,
@@ -95,20 +54,22 @@
       x: {
         title: {
           display: true,
-          text: xAxisText + ' (x)',
+          text: spec.graphSpecs[0].xAxisLabel + ' (x)',
           font: { size: 16 }
         }
       },
       y: {
         title: {
           display: true,
-          text: yFormula ? yAxisText + ` (${yFormula})` : yAxisText,
+          text: yFormula
+            ? `${spec.graphSpecs[0].yAxisLabel} (${yFormula})`
+            : spec.graphSpecs[0].yAxisLabel,
           font: { size: 16 }
         }
       }
     },
     hover: {
-      mode: isSizedSpec ? 'dataset' : undefined
+      mode: 'dataset'
     },
     plugins: {
       title: {
