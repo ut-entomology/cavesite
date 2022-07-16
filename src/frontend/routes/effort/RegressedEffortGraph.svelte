56bde69fcc2cf0b46630d54ec142adaf11c26a84
Joe Lapp <arachnojoe@gmail.com>
2022-07-16 10:31:36 -0500

    Fleshed out About the Average Model

--- src/frontend/routes/effort/RegressedEffortGraph.svelte
+++ src/frontend/routes/effort/RegressedEffortGraph.svelte
@@ -194,13 +194,49 @@
     regressed and modeled to fit an equation of the form <span class="eq"
       >y<sub>k</sub> = A x<sup>P</sup> + B</span
     >. The points
-    <span class="eq">(x, y<sub>k</sub>*W<sub>k</sub>/W<sub>T</sub>)</span> are then
-    plotted and a new model is generated from these points to fit the same equation.
-    Here, <span class="eq">W<sub>k</sub></span> is the weight of cave
-    <span class="eq">k</span>
-    and <span class="eq">W<sub>T</sub></span> is the sum of all
-    <span class="eq">W<sub>k</sub></span>, providing a weighted average of the
-    individual models.
+    <span class="eq"
+      >(x, (&Sigma; y<sub>k</sub>*w<sub>k</sub>)/(&Sigma; w<sub>k</sub>))</span
+    >
+    are then plotted, where <span class="eq">w<sub>k</sub></span> is the weight of cave
+    <span class="eq">k</span>, and a new model is generated from these points to fit the
+    same equation. The result is a weighted average of the individual cave models.
+
+    <ul>
+      <li>
+        The "Min. pts." input specifies the minimum number of points that must occur in
+        a cave for the cave's data to be included in the average model.
+      </li>
+      <li>
+        The "Min. x." input specifies the minimum value of x (visits or person-visits)
+        that the cave must have reached in order for the cave's data to be included in
+        the average model.
+      </li>
+      <li>
+        The "Weight" input provides the magnitude of the weighting <span class="eq"
+          >W</span
+        >, so that
+        <span class="eq"
+          >w<sub>k</sub>=<i>max</i>(&cup;{'{'}x<sub>ki</sub>{'}'})<sup>W</sup></span
+        >, where
+        <span class="eq">x<sub>ki</sub></span>
+        are the x values of cave <span class="eq">k</span>. Higher values of
+        <span class="eq">W</span> therefore bias the model toward caves having more visits
+        or person-visits (the x values).
+      </li>
+      <li>
+        The scatter plot and residuals graph only show data for caves that were included
+        in the average model. The scatter plot's title indicates both the number of
+        caves occuring in the average model and the total number of caves in the
+        cluster.
+      </li>
+      <li>
+        <b>IMPORTANT:</b> This modeling makes the largely false assumption that researchers
+        collect at least one specimen on each visit to each cave and deposit that specimen
+        in the UT Biospeleological collection; receipt of a specimen is what indicates a
+        visit to the cave. The resulting models therefore reflect both human behavior and
+        natural conditions.
+      </li>
+    </ul>
   </InfoDialog>
 {/if}
 
@@ -232,6 +268,13 @@
   }
   :global(.about_model_box) {
     font-size: 0.95rem;
+  }
+
+  ul {
+    margin: 1rem 0 -0.5rem 0;
+  }
+  li {
+    margin: 0.5rem 0;
   }
 
   .eq {
