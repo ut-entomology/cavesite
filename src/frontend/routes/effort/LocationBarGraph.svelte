e9a53cba49b68d99eb8cdd3d2a279f25429377f5
Joe Lapp <arachnojoe@gmail.com>
2022-07-17 16:48:04 -0500

    Started horizontal bars

---
+++ src/frontend/routes/effort/LocationBarGraph.svelte
@@ -0,0 +1,3 @@
+<script lang="ts">
+  import SplitHorizontalBar from './SplitHorizontalBar.svelte';
+</script>
---
+++ src/frontend/routes/effort/SplitHorizontalBar.svelte
@@ -0,0 +1,61 @@
+<script lang="ts" context="module">
+  export interface BarSplitSpec {
+    percent: number;
+    barColor: string;
+    backgroundColor: string;
+  }
+</script>
+
+<script lang="ts">
+  export let leftSplitSpec: BarSplitSpec | null;
+  export let rightSplitSpec: BarSplitSpec | null;
+</script>
+
+<div class="row">
+  {#if leftSplitSpec !== null}
+    {@const leftPercent = leftSplitSpec.percent}
+    <div class="col left_split">
+      <div class="full_bar" style="backgroundColor: {leftSplitSpec.barColor}">
+        {#if leftPercent > 0}
+          <div
+            class="value_bar"
+            style="width: {leftPercent}%; backgroundColor: {leftSplitSpec.backgroundColor}"
+          />
+        {/if}
+      </div>
+      <slot name="left" />
+    </div>
+  {/if}
+  {#if rightSplitSpec !== null}
+    {@const rightPercent = rightSplitSpec.percent}
+    <div class="col right_split">
+      <div class="full_bar" style="backgroundColor: {rightSplitSpec.barColor}">
+        {#if rightPercent > 0}
+          <div
+            class="value_bar"
+            style="width: {rightPercent}%; backgroundColor: {rightSplitSpec.backgroundColor}"
+          />
+        {/if}
+      </div>
+    </div>
+  {/if}
+</div>
+
+<style>
+  .left_split,
+  .right_split {
+    position: relative;
+  }
+
+  .full_bar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+
+  .value_bar {
+    height: 100%;
+  }
+</style>
