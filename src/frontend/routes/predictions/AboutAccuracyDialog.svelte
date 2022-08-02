d56c4841fa5bac9a2246a87dbd4e7681f132be72
Joe Lapp <arachnojoe@gmail.com>
2022-08-01 21:04:56 -0500

    More predictions how-to

--- src/frontend/routes/predictions/AboutAccuracyDialog.svelte
+++ src/frontend/routes/predictions/AboutAccuracyDialog.svelte
@@ -16,66 +16,10 @@
     per-cluster basis.
   </p>
 
+  <p>Be sure to have already read about how predictions are made.</p>
+
   <h3>Predicting numbers of additional species (+spp.)</h3>
 
-  <p>
-    Predictions of the number of additional species expected to be found are made
-    separately for the next visit to the cave and the next person-visit to the cave. The
-    person-visit prediction is the additional species expected to be found for each
-    person next visiting the cave. If the number of people participating in a collecting
-    trip significantly affects the number of species found, person-visit predictions
-    will be more accurate than visit predictions.
-  </p>
-  <p>
-    The prediction algorithm predicts the number of expected future species as a
-    function of the number of past species found. This entails counting species for all
-    records, whether the associated specimens are determined to species or not. The
-    minimum possible number of species is used, assuming the available determinations
-    are correct. For example, if one specimen is only determined to the order Araneae,
-    another to the family Hahniidae, another to the genus <i>Cicurina</i>, and another
-    to the species <i>Cicurina bandera</i>, the species count remains at 1 because this
-    species belongs to each of these higher ranks and it's possible that all of the
-    specimens are actually the same species. However, the addition of a second family, a
-    second genus, or a second species determination would increase the count.
-  </p>
-  <p>
-    To produce the number of additional species expected to be found at a cave, the
-    power curve <span class="eq">y</span> <span class="eq">=</span>
-    <span class="eq">Ax<sup>P</sup>+B</span> is fit to recent visits to the cave. Here,
-    <span class="eq">x</span>
-    is the number of visits or person-visits, <span class="eq">y</span> is the total
-    number of species found at the cave by the time of that visit or person-visit,
-    <span class="eq">A</span>
-    and <span class="eq">B</span> are constants determined by linear regression, and
-    <span class="eq">P</span> is a constant determined by a binary search of linear
-    regressions finding <span class="eq">A</span>, <span class="eq">B</span>, and
-    <span class="eq">P</span> having minimal RMSE. The curve is then extended to the next
-    visit or person visit, and the increase in the number of species is recorded as the prediction
-    of the number of additional species expected on the next visit or person-visit.
-  </p>
-  <p>
-    When configuring the clusters, you specify the minimum and maximum number of recent
-    visits to use for making predictions. This determines which points <span class="eq"
-      >(x,</span
-    > <span class="eq">y)</span> are used to produce the power curve. No fewer than 2 visits
-    can be used to make a prediction. If you allow 2-visit predictions and a cave has only
-    2 visits, the slope of a line through the points of those visits will be used as the
-    predicted number of additional species for that cave; the power curve is used when 3
-    or more visits are available. If the minimum number of visits you allow is N and the
-    cave has fewer than N points, no prediction is made for the number of additional species
-    expected to be found at the cave.
-  </p>
-  <p>
-    The greater the minimum number of visits used in making predictions, the more
-    accurate the predictions tend to be, but the fewer caves for which predictions can
-    be made about the expected number of additional species.
-  </p>
-  <p>
-    If you specify 'All' as maximum number of visits, a curve will be fit to all visits
-    to the cave at and above the minimum visits specified. Be aware that initial visits
-    to a cave tend to be highly productive, biasing the curve upwards and possibly
-    worsening predictions for future visits when species are actually leveling off.
-  </p>
   <p>
     The accuracy of these predictions is determined separately for each cluster. Within
     a cluster, caves are sorted by the number of additional species expected on the next
@@ -130,30 +74,6 @@
 
   <h3>Predicting next expected taxa (+taxa)</h3>
 
-  <p>
-    Caves are sorted into clusters by the similarity of their fauna under the assumption
-    that the more similar the fauna has been in the past, the more similar it will be in
-    the future. The additional fauna expected to be found in a cave is merely the fauna
-    found elsewhere in the cluster to which the cave belongs but not yet found in the
-    cave. For each cave, these remaining fauna are sorted by the number of visits to
-    other caves of the cluster in which their taxa have been found. The greater the
-    number of visits a taxon has, the higher the taxon appears in the sort, and
-    presumably the more likely the taxon will appear in future visits to the cave. The
-    taxonomic predictions therefore use historical prevalence to predict the taxa most
-    likely to be encountered next.
-  </p>
-  <p>
-    Predictions are not necessarily available for each taxonomic rank. They instead
-    suggest the most specific determinations that will be made for future specimens
-    according to the most specific determinations made for past specimens. For example,
-    if all past insects of a cluster have been determined to species, the cluster will
-    provide no prediction for the class Insecta, only predictions for species previously
-    determined; and if some past specimens were determined to Insecta but the cave does
-    not yet have a specimen with this incomplete determination, there will be a
-    prediction for future specimens only determined to class Insecta according to its
-    prevalence elsewhere in the cluster. Over time, as past specimens become further
-    determined, predicted taxa will become more specific too.
-  </p>
   <p>
     The accuracy of taxonomic predictions is measured similarly to the accuracy for the
     number of additional species expected. The process starts by first docking the {PREDICTION_HISTORY_SAMPLE_DEPTH}
