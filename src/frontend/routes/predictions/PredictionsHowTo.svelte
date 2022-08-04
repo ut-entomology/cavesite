45a49b1479aebcc5561913dbd27451d76e7abd7e
Joe Lapp <arachnojoe@gmail.com>
2022-08-03 22:37:32 -0500

    Some prediction how-to edits

--- src/frontend/routes/predictions/PredictionsHowTo.svelte
+++ src/frontend/routes/predictions/PredictionsHowTo.svelte
@@ -18,16 +18,16 @@
   This tab predicts the number of additional species you can expect to find on your next
   visit to a cave and the most likely taxa these additional species will have. It also
   predicts the number of additional species to expect per participating collector on the
-  next visit (per "person-visit"), in case these predictions are more accurate. The tab
+  next visit (per "person-visit"), in case such predictions are more accurate. The tab
   estimates the accuracy of its predictions according to the accuracy of the predictive
-  technique on recent historical data. You can also examine plots of numbers of species
-  found across visits and person-visits, regressions of these plots fit to power curves,
-  and the frequencies at which taxa are found on visits to caves.
+  technique on recent historical data. You can examine plots of numbers of species found
+  across visits and person-visits, regressions of these plots fit to power curves, and
+  the frequencies at which taxa are found on visits to caves.
 </p>
 <p>
   In order to limit the fauna and ensure predictions are meaningful, the analysis is
   restricted to caves. We do not have exact information about which locations are caves
-  and which are not, but most caves seem to include the word "cave" in their names, so
+  and which are not, but nearly all caves include the word "cave" in their names, so
   only locations that include the sequence of characters "cave" are included in the
   analysis. This includes, for example, names including the word "cavern".
 </p>
@@ -42,8 +42,8 @@
   fauna. We assume that the more similar the fauna, the more similar the habitat, and
   the more alike the caves. The more alike the caves of a cluster, the more accurately
   we should be able to make predictions about any cave in the cluster from information
-  about the other caves of the cluster. The accuracy achievable by applying the
-  predictive technique to historical data clustered this way suggests that these
+  about the other caves of the cluster. The accuracy that can be achieved by applying
+  the predictive technique to historical data clustered this way suggests that these
   assumptions are reasonable.
 </p>
 <p>
@@ -51,14 +51,14 @@
   fauna. You'll instead explore various clustering configurations to find one that meets
   your criteria. When you click the "Load Clusters" button (or the "Change Clusters"
   button if clusters are already loaded), you'll be given the following dialog box of
-  the most helpful parameters:
+  the most helpful clustering parameters:
 </p>
 
 <ExampleImage
   src="/predictions/default-predictions-config.png"
   alt="Default predictions configuration"
 />
-<p>The parameters are as follows:</p>
+<p>The clustering parameters are as follows:</p>
 <ul>
   <li>
     <b>Maximum clusters</b> into which to group the caves &mdash; This is the number of clusters
@@ -69,20 +69,22 @@
   </li>
   <li>
     <b>Fauna to compare</b> when clustering caves by common taxa &mdash; This selects the
-    fauna that will be used when comparing caves for similarity. "All fauna" uses all specimens
-    collected for each cave in the analysis. "Genera of cave obligates" uses only specimens
-    belonging to genera for which at least one species is known to be cave obligate, whether
-    or not the specimen is cave obligate. "Cave obligates" uses only specimens that are known
-    to be cave obligate. Generally, the more restricted the fauna, the more accurate the
-    predictions, but the fewer caves for which predictions are made.
+    fauna that will be used when comparing caves for similarity. The "All fauna" option uses
+    all specimens collected for each cave in the analysis. The "Genera of cave obligates"
+    option uses only specimens belonging to genera for which at least one species is known
+    to be cave obligate, whether or not the specimen is cave obligate. The "Cave obligates"
+    option uses only specimens that are known to be cave obligate. Generally, the more restricted
+    the fauna, the more accurate the predictions, but the fewer caves for which predictions
+    are made.
   </li>
   <li>
     <b>Highest taxonomic rank</b> to use in comparisons &mdash; This selects the taxonomic
-    ranks that are to contribute to the similarity of compared specimens. Each taxonomic
-    rank of each specimen is compared, up to the rank specified here. Lower taxonomic ranks
-    contribute more to the similarity measure than higher ranks, and ranks above the rank
-    selected here contribute nothing to similarity. Lower ranks tend to produce better predictions,
-    but mainly for well-sampled caves, leaving less-sampled caves poorly analyzed.
+    ranks that are to contribute to determinations of the similarity of compared specimens.
+    Each taxonomic rank of each specimen is compared, up to the rank specified here. Lower
+    taxonomic ranks contribute more to the similarity measure than higher ranks (they have
+    more weight in the algorithm), and ranks above the rank selected here contribute nothing
+    to similarity. Lower ranks tend to produce better predictions, but mainly for well-sampled
+    caves, leaving less-sampled caves poorly analyzed.
   </li>
   <li>
     <b>Min./max. recent visits</b> to use for making predictions &mdash; This specifies the
@@ -100,7 +102,8 @@
     whether to supplant the random assignment of caves to one of equally similar clusters
     with an assignment to the cluster whose centroid is nearest the cave. Caves of similar
     fauna need not be proximal, and caves of different fauna can be proximal, so rather than
-    always improving prediction results, using proximity tends to greatly randomize results.
+    always improving prediction results, using proximity can greatly randomize results. Consider
+    that some of the karst faunal zones track fault lines adjacent to other faunal zones.
     However, the fewer caves and the fewer taxa under analysis, the more using proximity
     seems to improve the accuracy of predicting taxa.
   </li>
@@ -128,10 +131,10 @@
   alt="Example title and summary of the clustering"
 />
 <p>
-  A description akin to the above appears at the top of the page. The title line
-  indicates the number of caves having the requested fauna and the number of clusters of
-  these caves the clustering was able to generate. Below the title line is a summary of
-  your chosen clustering configuration.
+  The top of the page has a description like the one above. The title line indicates the
+  number of caves having the requested fauna and the number of clusters of these caves
+  the clustering was able to generate. Below the title line is a summary of your chosen
+  clustering configuration.
 </p>
 <ExampleImage
   src="/predictions/accuracy-summary.png"
@@ -140,11 +143,11 @@
 <p>
   The above information describes the general accuracy of the clustering. It helps you
   assess whether you've produced a clustering that is accurate enough for your needs.
-  Click on the <span
+  You can click on the <span
     class="col link_text"
     on:click={() => (showingAboutAccuracy = true)}>about accuracy</span
-  > link (but not in the above example image) to learn how prediction accuracy is assessed
-  and the exact meanings of each of the provided summary measures.
+  > link to learn how prediction accuracy is assessed and the exact meanings of each of the
+  provided summary measures.
 </p>
 <ExampleImage
   src="/predictions/radar-graph.png"
@@ -152,7 +155,7 @@
 />
 <p>
   This radar graph shows you the percentage overlap in taxa among the clusters. Notice
-  that each cluster has its own color and number, the latter of which is preceded by a
+  that each cluster has its own color and number. Cluster numbers are preceded by a
   pound sign (#). Each radial line represents a cluster containing the indicated number
   of taxonomic determinations across all of its caves. The ten concentric circles
   represent percent overlap in increments of 10%. The intersection of a color of one
@@ -197,11 +200,11 @@
   The above set of images shows a series of interactions with the cave lookup box. The
   first image is what you see before you type anything into the box. You can type any
   portion of a cave name, and it will show up to {MAX_LOOKUP_MATCHES} caves whose names contain
-  the text you typed. This is what you see in the second image. When you click on one of
-  the caves in the list, the list disappears and you get the third image. Notice that a loupe
-  icon appears to the right of the box. Clicking on this loupe pops up a dialog box that
-  provides particulars about the cave, including the taxa next predicted to be found in the
-  cave and their estimated accuracies.
+  the text you typed, as seen in the second image. When you click on one of the caves in
+  the list, the list disappears and you get the third of the above images. Notice that a
+  loupe icon appears to the right of the box. Clicking on this loupe pops up a dialog box
+  that provides particulars about the cave, including the taxa next predicted to be found
+  in the cave and their estimated accuracies.
 </p>
 <ExampleImage src="/predictions/cluster-controls.png" alt="The cluster controls" />
 <p>
